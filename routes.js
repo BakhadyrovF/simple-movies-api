8934840954cedb2dbeb126eed5ef07229d696bd2
Bakhadyrov Firuzbek <bakhadyrovf@gmail.com>
2023-06-29 01:17:53 +0500

    ref: routes - prefix

--- routes.js
+++ routes.js
@@ -7,11 +7,11 @@ import MovieHandler from './http/handlers/MovieHandler.js';
 
 export const router = new Router();
 
-router.get('/movies', [MovieHandler, 'index']);
-router.get('/movies/{movieId}', [MovieHandler, 'show']);
-router.post('/movies', [MovieHandler, 'store']);
-router.put('/movies/{movieId}', [MovieHandler, 'update']);
-router.delete('/movies/{movieId}', [MovieHandler, 'delete'])
+router.get('/api/movies', [MovieHandler, 'index']);
+router.get('api//movies/{movieId}', [MovieHandler, 'show']);
+router.post('/api/movies', [MovieHandler, 'store']);
+router.put('/api/movies/{movieId}', [MovieHandler, 'update']);
+router.delete('/api/movies/{movieId}', [MovieHandler, 'delete'])
 
 
 
